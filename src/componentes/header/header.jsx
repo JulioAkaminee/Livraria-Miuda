1a1f48ccb8ad02914782cdb6d0d77a90a3483626
JulioAkaminee <juliooakamine@gmail.com>
2024-09-05 11:05:53 -0300

    feito o menu burguer

--- src/componentes/header/header.jsx
+++ src/componentes/header/header.jsx
@@ -1,33 +1,48 @@
-import '../header//header.css'
+import React, { useState } from 'react';
+import '../header/header.css';
 
 function Header() {
+  // Estado para controlar a visibilidade do menu
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  // Função para alternar a visibilidade do menu
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <div>
-        <header>
-          <a href="../header/header.jsx"><img src="../assets/images/logo-miuda.png" alt="" /></a>
+      <header>
+        <a href="../header/header.jsx">
+          <img src="../assets/images/logo-miuda.png" alt="Logo" />
+        </a>
         
-          <div className="search-container">
+        <div className="search-container">
           <input type="search" placeholder="Pesquisar..." />
           <span className="material-symbols-outlined">search</span>
         </div>
         
-          <div className='icones-header'>
-            {/* icone de favorito */}
-            <span className="material-symbols-outlined">favorite</span>
-            {/* icone de carrinho */}
-            <span className="material-symbols-outlined">shopping_cart</span>
-            {/* icone de conta */}
-            <span className="material-symbols-outlined">person</span>
-          </div>
-          <span class="material-symbols-outlined" id='menu'>menu</span>
-        </header>
-          <div className='menu-header'>
-            <ul>
-                <a href=""><li>Favoritos</li></a>
-                <a href=""> <li>Carrinho</li></a>
-                <a href=""><li>Café</li> </a>
-            </ul>
-          </div>
+        <div className='icones-header'>
+          <span className="material-symbols-outlined">favorite</span>
+          <span className="material-symbols-outlined">shopping_cart</span>
+          <span className="material-symbols-outlined">person</span>
+        </div>
+        
+        <span className="material-symbols-outlined" id='menu' onClick={toggleMenu}>
+          menu
+        </span>
+      </header>
+      
+      {/* Menu Header - Visível quando isMenuOpen é true */}
+      {isMenuOpen && (
+        <div className='menu-header'>
+          <ul>
+            <li><a href="">Favoritos</a></li>
+            <li><a href="">Carrinho</a></li>
+            <li><a href="">Café</a></li>
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
